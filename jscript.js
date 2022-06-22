c4ce713280c382c98fc162883973f8fc6296ad68
Kaitlyn Hardin <khardin9@my.apsu.edu>
2022-06-21 22:40:46 -0500

    Add functionality to all number buttons, decimal, clear, and delete in JS script.

--- jscript.js
+++ jscript.js
@@ -1,9 +1,60 @@
-numbers = document.querySelectorAll(".numbers-container");
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator-keys");
+const display = document.querySelector(".calculator-display");
 
-console.log(numbers);
+//Add click eventListener to all buttons in .calculator-keys class
+keys.addEventListener("click", e => {
+    if(e.target.matches('button'))
+    {
+      const currentKey = e.target;
+      const action = currentKey.dataset.action; //gets data-action of key
 
-numbers.forEach((button) => button.addEventListener("click", () => 
-    {if(button.textContent == 7)    
-        {console.log(button.textContent)}
+      if(action === "all-clear")
+      {
+          console.log("all clear");
+      }
+      if(action === "clear")
+      {
+          display.textContent = "0";
+      }
+      if(action === "delete")
+      {
+         const displayText = display.textContent;
+         display.textContent = displayText.slice(0, -1);
+      }
+      if(action === "modulus" || 
+      action === "squareRoot" ||
+      action === "exponent" ||
+      action === "divide" ||
+      action === "multiply" || 
+      action === "subtract"||
+      action === "add")
+      {
+          console.log("key operation")
+      }
+      if(action === "decimal")
+      {
+        display.textContent += ".";  
+      }
+      if(action === "sign")
+      {
+          console.log("sign change");
+      }
+      if(action === "equal")
+      {
+          console.log("equal");
+      }
+
+      if(!action)
+      {
+          console.log("not action");
+          if(display.textContent === "0")
+          {
+            display.textContent = currentKey.textContent;
+          }
+          else{
+            display.textContent += currentKey.textContent;  
+          }
+      }
+    }
 })
-);
